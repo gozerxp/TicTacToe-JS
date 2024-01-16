cacbae44e0e023b32ccb6a6da3b17cfb3e0687b9
Dan Andersen <gozerxp@gmail.com>
2024-01-16 01:41:34 -0500

    update

--- src/score.js
+++ src/score.js
@@ -36,29 +36,30 @@ export const score = {
             font_size: 40
         };
 
-        this.draw_score_component(ctx, assets.x, this.x, 0, param);
-        this.draw_score_component(ctx, assets.o, this.o, 1, param);
-        this.draw_score_component(ctx, assets.cats, this.cats, 2, param);
-
-    },
-
-    draw_score_component: function (ctx, img, score, position, param) {
-
-        ctx.font = `${param.font_size}px '${settings.font_face}'`;
-
-        const txt_y = ctx.canvas.height / 2 + param.font_size / 2 - param.margin / 2;
-        const item_width = param.width + param.margin + ctx.measureText(`${score}`).width;
-        const x_pos = (ctx.canvas.width / 3) * position + (ctx.canvas.width / 3) / 2 - item_width / 2;
-
-        ctx.drawImage(img, x_pos, 0, param.width, param.height);
-
-        const offset = -1;
-        ctx.fillStyle = "white";
-        ctx.fillText(`${score}`, x_pos + param.margin + param.width + offset, txt_y + offset);
-
-        ctx.fillStyle = settings.COLOR;
-        ctx.fillText(`${score}`, x_pos + param.margin + param.width, txt_y);
+        draw_score_component(ctx, assets.x, this.x, 0, param);
+        draw_score_component(ctx, assets.o, this.o, 1, param);
+        draw_score_component(ctx, assets.cats, this.cats, 2, param);
 
     }
 
 };
+
+function draw_score_component (ctx, img, score, position, param) {
+
+    ctx.font = `${param.font_size}px '${settings.font_face}'`;
+
+    const txt_y = ctx.canvas.height / 2 + param.font_size / 2 - param.margin / 2;
+    const item_width = param.width + param.margin + ctx.measureText(`${score}`).width;
+    const x_pos = (ctx.canvas.width / 3) * position + (ctx.canvas.width / 3) / 2 - item_width / 2;
+
+    ctx.drawImage(img, x_pos, 0, param.width, param.height);
+
+    const offset = -1;
+    ctx.fillStyle = "white";
+    ctx.fillText(`${score}`, x_pos + param.margin + param.width + offset, txt_y + offset);
+
+    ctx.fillStyle = settings.COLOR;
+    ctx.fillText(`${score}`, x_pos + param.margin + param.width, txt_y);
+
+}
+
